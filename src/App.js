0f43963521b4ec2f3d0a61fad72f24fd11eca037
Nitish Bharti <nb88640@gmail.com>
2024-03-06 23:55:54 +0530

    button color changes done

--- src/App.js
+++ src/App.js
@@ -62,9 +62,9 @@ function App() {
     background:"red",
     boxShadow:
       "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
-    borderRadius: '14px', // Rounded corners
-    border: 'none', // No border
-    padding: '10px', // Adjust padding as needed
+    borderRadius: '14px', 
+    border: 'none', 
+    padding: '10px',
     boxSizing: 'border-box',
   }
 
