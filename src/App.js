9e06cebdd568dc44b28930603f6966a75a2fc5cf
Nitish Bharti <nb88640@gmail.com>
2024-03-06 23:38:06 +0530

    ui intigration is on the way

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const imageUrl = "/background.jpg";
+
 function App() {
   const [data, setData] = useState(null);
+
+  const mainDivStyle = {
+    margin: 0,
+    padding: 0,
+    display: "flex",
+    backgroundImage: `url(${imageUrl})`,
+    backgroundSize: "cover",
+    height: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  const card = {
+    display: "flex",
+    flexDirection: "column",
+    // justifyContent: "center",
+    alignItems: "center",
+    width: "500px",
+    height: "500px",
+    gap:"5px",
+    background: "rgb(227 227 227 / 26%)",
+    boxShadow:
+      "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+  };
+  const headingTag = {
+    fontSize: "25px",
+    marginTop:"50px",
+ 
+  };
+  const location = {
+    marginTop:"5px",
+    width : "450px",
+    height: "30px",
+    boxShadow:
+      "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+    borderRadius: '14px', // Rounded corners
+    border: 'none', // No border
+    padding: '10px', // Adjust padding as needed
+    boxSizing: 'border-box',
+  }
 
   useEffect(() => {
     async function fetchWeather() {
@@ -23,8 +64,18 @@ function App() {
   console.log("Data:", data);
 
   return (
-    <div className="app">
-      {data ? (
+    <div className="app" style={mainDivStyle}>
+      <div className="card" style={card}>
+        <div className="heading tag" style={headingTag}>
+         
+          Please Enter the Location and Phone number
+        </div>
+        <div><input name="location" style={location} ></input></div>
+        <div><input name="phoneNumber" style={location} ></input></div>
+        <div>Button</div>
+      </div>
+
+      {/* {data ? (
         <div>
           <p>City: {data.city}</p>
           <p>Weather: {data.weather}</p>
@@ -41,7 +92,7 @@ function App() {
         </div>
       ) : (
         <p>Loading...</p>
-      )}
+      )} */}
     </div>
   );
 }
